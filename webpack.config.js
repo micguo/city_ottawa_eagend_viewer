69bea282e84b910850c3f71bbca4a63f8c4f91eb
micguo <micguo2000@gmail.com>
2017-06-12 15:36:38 -0400

    Bugfix - webpack resolve path

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     ],
     resolve: {
         modules: [
-            path.join(__dirname, 'src'),
+            path.join(__dirname, 'src/server'),
             'node_modules'
         ]
     },
