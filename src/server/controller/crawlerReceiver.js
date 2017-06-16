1bbf36d9c050e41aa38c51abb0270b51bddfadc1
Hao Guo <micguo2000@gmail.com>
2017-06-15 22:11:29 -0400

    Use upsert for meeting entity

--- src/server/controller/crawlerReceiver.js
+++ src/server/controller/crawlerReceiver.js
@@ -8,19 +8,10 @@ router.post('/', function(req, res) {
         res.status(400).json('Post object is not an array');
     } else {
         req.body.forEach(function(value) {
-            // FIXME, use upsert here
-            let meeting = new Meeting();
-            meeting.name = value.type;
-            meeting.id = value.id;
-            meeting.date = value.date;
-
             // save the bear and check for errors
-            meeting.save(function(err) {
+            Meeting.update({id: value.id}, value, {upsert: true}, function(err) {
                 if (err) {
-                    if(err.code === 11000)
-                        res.status(409).json({error: "exist id"});
-                    else
-                        res.send(err);
+                    res.send(err);
                 }
             });
         });
