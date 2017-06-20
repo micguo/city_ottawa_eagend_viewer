d4ab2543c33aeb5c17985478ded1067405705f0d
micguo <micguo2000@gmail.com>
2017-06-20 15:59:25 -0400

    Add css-loader and style-loader

--- src/client/components/layout.jsx
+++ src/client/components/layout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM   from 'react-dom';
 import AppHeader from './Header/index.jsx';
 import AppFooter from './Footer/index.jsx';
+import Drawer from './Drawer/index.jsx';
+import styles from './layout.css';
 
 ReactDOM.render(
-    <div>
+    <div id="body-content">
         <header><AppHeader/></header>
-        <div>Content</div>
+        <Drawer />
         <footer><AppFooter/></footer>
     </div>,
     document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,10 @@ module.exports = [{
                 query: {
                     presets: ['es2015', 'react']
                 }
+            },
+            {
+                test: /.css$/,
+                loader: ['style-loader', 'css-loader']
             }
         ]
     },
