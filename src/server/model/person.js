900accfb4e2b351e227edaac80f3e7d1b042dd96
micguo <micguo2000@gmail.com>
2017-08-10 16:37:40 -0400

    working

--- src/server/model/person.js
+++ src/server/model/person.js
@@ -1,8 +1,12 @@
 import mongoose from "./mongoose";
 
 let PersonSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
-});
+        firstName: String,
+        lastName: String
+    },
+    {
+        collection : 'person'
+    }
+);
 
 export default mongoose.model('Person', PersonSchema);
